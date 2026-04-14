7b5b4e2c5b23aa466398bafc518209cf8d140243
ah-rony <ahrony1122@gmail.com>
2026-04-14 22:49:19 +0600

    updated & project ongoing not finished yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
+/*
+Game 
+  --> Board
+      -> Square
+  --> History
+*/
 
-const App = () => {
+import { useState } from "react"
+
+
+function Square(){
+  const [value, setValue] = useState(null)
+
+  function handleClick(){
+    setValue('X')    
+  }
+
+  return  (
+    <button
+        className='bg-white border border-gray-400 w-12 h-12 m-1 leading-9 text-lg'
+        onClick={handleClick}>
+        {value}
+    </button>)
+
+}
+  
+
+const Board = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null))
+  
   return (
-    <div>
-      <h1 className='font-bold underline'>Hello Tic Tac Toe</h1>
+    <>
+    <div className="flex">
+      <Square value = {squares[0]} />
+      <Square value = {squares[1]} />
+      <Square value = {squares[2]} />
     </div>
+    <div className="flex">
+      <Square value = {squares[3]} />
+      <Square value = {squares[4]} />
+      <Square value = {squares[5]} />
+    </div>
+    <div className="flex">
+      <Square value = {squares[6]} />
+      <Square value = {squares[7]} />
+      <Square value = {squares[8]} />
+    </div>
+    </>
   )
 }
 
-export default App
+export default Board
